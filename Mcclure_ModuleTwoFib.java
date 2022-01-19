b4cf37ea0efde30a08ca8aa70389589514e4de69
hezekiahes <59317644+hezekiahes@users.noreply.github.com>
2022-01-19 18:52:03 -0500

    Add files via upload
    Revision 1 includes insertion of javadoc

--- Mcclure_ModuleTwoFib.java
+++ Mcclure_ModuleTwoFib.java
@@ -1,7 +1,11 @@
 
 public class Mcclure_ModuleTwoFib {
 
-	
+	/**
+	 * Method to calculate the nth term of the fibonacci sequence
+	 * @param n  
+	 * @return nth term of fibonacci sequence
+	 */
 	public static int fib(int n) {
 		
 		if (n <= 1) 
@@ -10,12 +14,15 @@ public class Mcclure_ModuleTwoFib {
 		return fib(n - 1) + fib(n - 2);  
 	}
 	
-	
+	/**
+	 * Main method 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		int n = 10;
 		
-		System.out.println("The nth term of the Fibonacci Sequence is " + fib(n));
+		System.out.println("The " + n + "th term of the Fibonacci Sequence is " + fib(n));
 		
 	}
 }
